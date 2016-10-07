e567a4fbfcd7a0b0dc4c14ea568193f3d67e66c1
Thomas Murray <first.hadron@gmail.com>
2016-10-07 21:12:28 +0100

    Week2 Update

---
+++ Week2/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Starting BackUp
+
+SRCDIR=/backup
+
+DESDIR=/Week1
+
+tar-cpzf$DESDIR/$backup.tar $SRCDIR
+
+
+echo Finishing BackIp
+
---
+++ Week2/week2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Script Name: "$0"
+
+echo Total Number of Argument Passed: "$#"
+
+echo Arguments List -
+echo 1. $1 is this correct?
+echo 2. $2 is this correct?
+echo 3. $3 is this correct?
+
+echo ALL Arguments are: "$*"
+
